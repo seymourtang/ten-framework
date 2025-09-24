ba0473eea1b38281115cde8499359b48f6c6854b
Xianmeng Tang <tangxianmeng@live.com>
2025-09-24 17:39:57 +0800

    fix: include current_turn_id in TTS audio start (#1557)
    * fix: include current_turn_id in TTS audio start metrics
    
    * chore: bump version to 0.2.7 in cartesia_tts2 manifest

--- ai_agents/agents/ten_packages/extension/cartesia_tts2/extension.py
+++ ai_agents/agents/ten_packages/extension/cartesia_tts2/extension.py
@@ -300,7 +300,8 @@ class CartesiaTTSExtension(AsyncTTS2BaseExtension):
                             self.sent_ts = datetime.now()
                             ttfb = data_msg
                             await self.send_tts_audio_start(
-                                self.current_request_id
+                                self.current_request_id,
+                                self.current_turn_id,
                             )
                             await self.send_tts_ttfb_metrics(
                                 self.current_request_id,
